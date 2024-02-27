45f33675a0b3afff3d257ec832ff088e349cf2cc
Сергей Субботин <zarodiny@yandex.ru>
2024-02-27 16:50:00 +1000

    feat: добавляет стили

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var target = document.querySelector('.burger-icon');
-    var isClosed = true;
+    const target = document.querySelector('.burger-icon');
+    const headerNav = document.querySelector(".header-nav");
+
+    let isClosed = true;
 
     target.addEventListener('click', function () {
         burgerTime();
@@ -10,10 +12,12 @@ document.addEventListener('DOMContentLoaded', function () {
         if (isClosed) {
             target.classList.remove('open-burger');
             target.classList.add('closed-burger');
+            headerNav.classList.add('is-active');
             isClosed = false;
         } else {
             target.classList.remove('closed-burger');
             target.classList.add('open-burger');
+            headerNav.classList.remove('is-active');
             isClosed = true;
         }
     }
