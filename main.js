1c1cb56fb3488429e50c84d2dd720d27d6ba1a10
Сергей Субботин <zarodiny@yandex.ru>
2024-02-28 17:09:35 +1000

    feat: добавляет секции

--- main.js
+++ main.js
@@ -37,4 +37,104 @@ document.addEventListener('DOMContentLoaded', function () {
             },
         });
     }
+
+    // Swiper-team
+    const teamSlider = () => {
+        const itemSlider = function () {
+            const item = document.querySelector('.team-item');
+            let swiper;
+            // console.log('Подключился')
+            if (item) {
+
+                swiper = new Swiper(item, {
+                    loop: true,
+                    autoplay: {
+                        delay: 10000
+                    },
+                    speed: 700,
+                    direction: 'horizontal',
+                    spaceBetween: 15,
+                    slidesPerView: 1,
+                    equalHeight: true,
+
+                    // If we need pagination
+                    navigation: {
+                        nextEl: ".arrow__next--team",
+                        prevEl: ".arrow__prev--team",
+                    },
+                    breakpoints: {
+                        320: {
+                            slidesPerView: 1.5,
+                            spaceBetween: 15
+                        },
+                        769: {
+                            slidesPerView: 3,
+                            spaceBetween: 15
+                        },
+                        1200: {
+                            slidesPerView: 4,
+                            spaceBetween: 10
+                        }
+                    }
+                });
+            }
+        };
+
+        itemSlider();
+    }
+
+    // Swiper-team
+    const smileSlider = () => {
+        const itemSlider = function () {
+            const item = document.querySelector('.smile-item');
+            let swiper;
+            // console.log('Подключился')
+            if (item) {
+
+                swiper = new Swiper(item, {
+                    loop: true,
+                    autoplay: {
+                        delay: 10000
+                    },
+                    speed: 700,
+                    direction: 'horizontal',
+                    spaceBetween: 15,
+                    slidesPerView: 1,
+                    equalHeight: true,
+
+                    // If we need pagination
+                    navigation: {
+                        nextEl: ".arrow__next--smile",
+                        prevEl: ".arrow__prev--smile",
+                    },
+                    breakpoints: {
+                        320: {
+                            slidesPerView: 1,
+                            spaceBetween: 15
+                        },
+                        695: {
+                            slidesPerView: 1,
+                            spaceBetween: 15
+                        },
+                        767: {
+                            slidesPerView: 2,
+                            spaceBetween: 15
+                        },
+                        1200: {
+                            slidesPerView: 2,
+                            spaceBetween: 10
+                        }
+                    }
+                });
+            }
+        };
+
+        itemSlider();
+    }
+
+    teamSlider();
+    smileSlider();
+
+
+    // DOM
 });
